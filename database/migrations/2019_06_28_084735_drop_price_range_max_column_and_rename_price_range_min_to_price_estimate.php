38aef92b5d8be06cb8a759456d46bde06764786a
johnGachihi <John.Waithaka@O365.strathmore.edu>
2019-06-28 12:45:32 +0300

    Created migration to drop price_range_max column and rename price_range_min to price_estimate

---
+++ database/migrations/2019_06_28_084735_drop_price_range_max_column_and_rename_price_range_min_to_price_estimate.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropPriceRangeMaxColumnAndRenamePriceRangeMinToPriceEstimate extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('goods', function (Blueprint $table) {
+            $table->dropColumn('price_range_max');
+//            $table->renameColumn('price_range_min', 'price_estimate');
+            DB::statement("ALTER table goods CHANGE price_range_min price_estimate int");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('goods', function (Blueprint $table) {
+            $table->integer('price_range_min')->nullable();
+            DB::statement("ALTER table goods CHANGE price_estimate price_range_min int");
+        });
+    }
+}
